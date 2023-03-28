ed510390891a1b844f9d5dea5737f7e269329d95
Alan Cleverson <117549807+Alan-Cleverson@users.noreply.github.com>
2023-03-28 18:47:41 -0300

    Atz

--- ex014/script.js
+++ ex014/script.js
@@ -9,7 +9,7 @@ function carregar() {
 
     if (horas >= 0 && horas < 12){
         //Bom dia
-        msg.innerHTML = 'Bom dia, agora são <strong width = "2em">'+ horas +':'+ min +'</strong>'
+        msg.innerHTML = 'Bom dia, agora são <strong>'+ horas +':'+ min +'</strong>'
         img.src = 'manha.png'
         document.body.style.background = '#c0b182'
         } else if (horas >= 12 && horas <= 18){
@@ -19,7 +19,7 @@ function carregar() {
             document.body.style.background = '#ba6d4c'
         } else {
             //Boa noite
-            msg.innerHTML = 'Boa noit, agora são  <strong>'+ horas +':'+ min +'</strong>'
+            msg.innerHTML = 'Boa noite, agora são  <strong>'+ horas +':'+ min +'</strong>'
             img.src = 'noite.png'
             document.body.style.background = '#162b36'
         }
